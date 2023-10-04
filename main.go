4250c7843a0a071352968e060ba35ce0a7e295b2
AndyXT <axtreto@me.com>
2023-10-03 21:15:28 -0400

    stuff

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "fmt"
-    "cryptopal-go/pkg/hex"
+		"cryptopal-go/pkg/hex"
+		"fmt"
 )
 
 func main() {
@@ -15,9 +15,7 @@ func challenge1_1() {
     hexStr := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     b64 := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
-    var b64Str string
-
-    b64Str = hex.ToBase64(hexStr)
+		b64Str := hex.ToBase64(hexStr)
 
     fmt.Printf("%s", b64Str)
     if b64 != b64Str {
--- pkg/hex/hex.go
+++ pkg/hex/hex.go
@@ -1,51 +1,52 @@
 package hex
 
 import (
-    "fmt"
-    "cryptopal-go/pkg/bytes"
+	"cryptopal-go/pkg/bytes"
+	"fmt"
 )
+
 func ToBase64(hexStr string) string {
-    var base64 string
-    hexStrSize := len(hexStr)
-    hexStrSlice := []rune(hexStr)
+	var base64 string
+	hexStrSize := len(hexStr)
+	hexStrSlice := []rune(hexStr)
 
-    if hexStrSize % 2 != 0 {
-        fmt.Println("error: hexToBase64")
-    }
+	if hexStrSize%2 != 0 {
+		fmt.Println("error: hexToBase64")
+	}
 
-    hexToBytes := ToByteArray(hexStrSlice)
+	hexToBytes := ToByteArray(hexStrSlice)
 
-    base64 = bytes.ToBase64Str(hexToBytes)
+	base64 = bytes.ToBase64Str(hexToBytes)
 
-    return base64
+	return base64
 }
 
 func ToByteArray(hexStrSlice []rune) []byte {
-    bytesSlice := make([]byte, 0, len(hexStrSlice)/2)
+	bytesSlice := make([]byte, 0, len(hexStrSlice)/2)
 
-    for i := 0; i < len(hexStrSlice); i += 2 {
-        char1 := hexStrSlice[i]
-        char2 := hexStrSlice[i+1]
-        byteVal := CharToByte(char1) * 16 + CharToByte(char2)
-        bytesSlice = append(bytesSlice, byteVal)
-    }
+	for i := 0; i < len(hexStrSlice); i += 2 {
+		char1 := hexStrSlice[i]
+		char2 := hexStrSlice[i+1]
+		byteVal := CharToByte(char1)*16 + CharToByte(char2)
+		bytesSlice = append(bytesSlice, byteVal)
+	}
 
-    return bytesSlice
+	return bytesSlice
 }
 
 func CharToByte(char rune) byte {
-    var hexByte byte
+	var hexByte byte
 
-    switch {
-    case char >= '0' && char <= '9':
-        hexByte = byte(char) - byte('0')
-    case char >= 'a' && char <= 'f':
-        hexByte = byte(char) - byte('a') + 10
-    case char >= 'A' && char <= 'F':
-        hexByte = byte(char) - byte('A') + 10
-    default:
-        fmt.Println("error: CharToByte")
-    }
+	switch {
+	case char >= '0' && char <= '9':
+		hexByte = byte(char) - byte('0')
+	case char >= 'a' && char <= 'f':
+		hexByte = byte(char) - byte('a') + 10
+	case char >= 'A' && char <= 'F':
+		hexByte = byte(char) - byte('A') + 10
+	default:
+		fmt.Println("error: CharToByte")
+	}
 
-    return hexByte
+	return hexByte
 }
