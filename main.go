56be4a96c08603dd7075afced8c36ca6da65d9d1
AndyXT <axtreto@me.com>
2023-10-03 21:20:22 -0400

    fixing formatting

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-		"cryptopal-go/pkg/hex"
-		"fmt"
+	"cryptopal-go/pkg/hex"
+	"fmt"
 )
 
 func main() {
-    fmt.Println("vim-go")
+	fmt.Println("vim-go")
 
-    challenge1_1()
+	challenge1_1()
 }
 
 func challenge1_1() {
-    hexStr := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-    b64 := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+	hexStr := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	b64 := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
-		b64Str := hex.ToBase64(hexStr)
+	b64Str := hex.ToBase64(hexStr)
 
-    fmt.Printf("%s", b64Str)
-    if b64 != b64Str {
-        fmt.Println("Error: challenge1")
-    }
+	fmt.Printf("%s", b64Str)
+	if b64 != b64Str {
+		fmt.Println("Error: challenge1")
+	}
 }
--- pkg/bytes/bytes.go
+++ pkg/bytes/bytes.go
@@ -1,23 +1,23 @@
 package bytes
 
 func ToBase64Str(bytes []byte) string {
-    base64Alphabet := [64]rune{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'}
-    b64Str := make([]rune, 0, len(bytes)*4/3)
+	base64Alphabet := [64]rune{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'}
+	b64Str := make([]rune, 0, len(bytes)*4/3)
 
-    u32Slice := make([]uint32, 0, len(bytes)/3)
-    for i := 0; i < len(bytes); i += 3 {
-        val := uint32(bytes[i]) << 16
-        val |= uint32(bytes[i+1]) << 8
-        val |= uint32(bytes[i+2])
-        u32Slice = append(u32Slice, val)
-    }
+	u32Slice := make([]uint32, 0, len(bytes)/3)
+	for i := 0; i < len(bytes); i += 3 {
+		val := uint32(bytes[i]) << 16
+		val |= uint32(bytes[i+1]) << 8
+		val |= uint32(bytes[i+2])
+		u32Slice = append(u32Slice, val)
+	}
 
-    for i := 0; i < len(u32Slice); i++ {
-        for j := 0; j < 4; j++ {
-            var idx byte = byte((u32Slice[i] >> (18 - j * 6)) & 0x3F)
-            b64Str = append(b64Str, base64Alphabet[idx])
-        }
-    }
+	for i := 0; i < len(u32Slice); i++ {
+		for j := 0; j < 4; j++ {
+			var idx byte = byte((u32Slice[i] >> (18 - j * 6)) & 0x3F)
+			b64Str = append(b64Str, base64Alphabet[idx])
+		}
+	}
 
-    return string(b64Str)
+	return string(b64Str)
 }
