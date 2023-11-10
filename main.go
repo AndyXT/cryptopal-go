ef13375d495f890220937af8c0f02a4e0c569547
AndyXT <axtreto@me.com>
2023-11-10 15:10:18 -0500

    using go std library instead

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"cryptopal-go/pkg/encoding"
 	"cryptopal-go/pkg/hex"
 	"cryptopal-go/pkg/util"
+	"encoding/base64"
+	hx "encoding/hex"
 	"errors"
 	"fmt"
 )
@@ -36,7 +38,12 @@ func challenge1_1() error {
 	hexStr := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 	b64 := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
-	b64Str := hex.ToBase64(hexStr)
+	// b64Str := hex.ToBase64(hexStr)
+	hexArray, err := hx.DecodeString(hexStr)
+	if (err != nil) {
+		fmt.Println(err)
+	}
+	b64Str := base64.StdEncoding.EncodeToString(hexArray)
 
 	fmt.Printf("%s\n", b64Str)
 	if b64 != b64Str {
